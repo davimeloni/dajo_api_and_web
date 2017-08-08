96429a44aaee69b876033a52a32badfd6ad4dc35
davimeloni <davimeloni>
2017-08-07 21:00:57 -0300

    acces-origin

--- app.js
+++ app.js
@@ -34,6 +34,13 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+app.use(function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', "*");
+    res.header('Access-Control-Allow-Methods','GET,PUT,POST,DELETE'); 
+    res.header('Access-Control-Allow-Headers', 'Content-Type'); 
+    next();
+})
+
 //log every request
 app.use(function (req, res, next) {
     //print log
