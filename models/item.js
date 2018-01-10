216bfc976bc617ab7d3e8c60ea708251f9319418
unknown <davimeloni>
2018-01-10 13:26:56 -0200

    description unrequired

--- models/item.js
+++ models/item.js
@@ -29,8 +29,7 @@ var itemSchema = new mongoose.Schema({
         required: true
     },
     description: {
-        type: String,
-        required: true
+        type: String
     },
     featured: {
         type: Boolean,
